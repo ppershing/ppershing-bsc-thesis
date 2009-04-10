32ca9683d9e9582ecf5687857e6d45ddfc2d12e1
ppershing <ppershing@5c1f145d-967f-e645-29ce-97e097377ead>
2009-04-10 16:09:27 +0000

    obrazky k jpeg kompresii
    git-svn-id: http://ppershing-bsc-thesis.googlecode.com/svn@64 5c1f145d-967f-e645-29ce-97e097377ead

---
+++ obrazky/informatika/image_compression/dct_dft_energy.py
@@ -0,0 +1,34 @@
+from dct_dft import DCT,DFT
+from pnm import PNM
+import cmath
+import math
+import colorschemes
+from histogram import Histogram
+
+image = PNM.loadToGreyscale('image.ppm')
+
+image = [[ x-0.0 for x in row] for row in image]
+
+dct = DCT.transform2D(image)
+dft = DFT.transform2D(image)
+
+def norm(x):
+    return math.log(1+abs(x))
+
+koef =1.0/math.log(1+abs(dct[0][0])) # is the same as dft[0][0]
+
+dct_energy = [[math.log(1+abs(x))*koef for x in row] for row in dct]
+dft_energy = [[math.log(1+abs(x))*koef for x in row] for row in dft]
+
+
+k = 200 / 7.0
+dct_histogram = Histogram.MakeHistogramImage(\
+        reduce(lambda x,y:x+y,dct_energy), 200, 300, k)
+dft_histogram = Histogram.MakeHistogramImage(\
+        reduce(lambda x,y:x+y,dft_energy), 200, 300, k)
+
+PNM.saveColor('dct_histogram.ppm', dct_histogram, colorschemes.InverseGray)
+PNM.saveColor('dft_histogram.ppm', dft_histogram, colorschemes.InverseGray)
+
+PNM.saveColor('dct.ppm', dct_energy, colorschemes.BlueGreenRed)
+PNM.saveColor('dft.ppm', dft_energy, colorschemes.BlueGreenRed)
